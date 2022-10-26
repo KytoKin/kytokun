b43166073ba36a30b143b70ba67b10325b63d09e
KytoKin <80213131+KytoKin@users.noreply.github.com>
2022-10-27 06:33:03 +0700

    Initialize master

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: npm install -g pm2 && pm2-runtime index.js
+worker: node . --db 'mongodb+srv://Kiyotakakin:13082003@cluster0.anpgxcl.mongodb.net/?retryWrites=true&w=majority'
---
+++ config.js
@@ -0,0 +1,137 @@
+let fs = require('fs')
+global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to /src/owner.json
+global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
+global.thumbt = global.img
+global.fsx = 999999999999
+global.kontak = [
+['6283172999950', 'Kyto Kin', true], 
+['6283172999950', 'TOLONG JGN SPAM', true] 
+]
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  neoxr: 'https://api.neoxr.eu.org',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  males: 'https://malesin.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.neoxr.eu.org': 'yntkts',
+  'https://api.xteam.xyz': 'apikeyaine',
+  'https://melcanz.com': 'elaina',
+  'https://api.lolhuman.xyz': 'kytokin',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+global.linkyt = 'https://youtu.be/RlhkqeUDZrA'
+global.WM = '                「 Ky-Bot MD あ⁩ 」'
+global.linkgc = 'https://chat.whatsapp.com/FsxACLENxKo4jeqrO3t2k1'
+global.linkig = 'https://www.instagram.com/neko_shop01'
+global.deslink = ''
+global.logo = 'https://telegra.ph/file/dffdaf0e8d59ad104e526.jpg'
+global.titlink = 'Folllow me on Instagram'
+global.bodlink = '@neko_shop01'
+
+global.wait = '```「▰▰▰▱▱▱▱▱▱▱」Loading...```'
+global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
+
+//========Url Template Buttons==========//
+global.dtu = '🌏 GROUP OFFICIAL'
+global.urlnya = global.linkgc
+
+//============= callButtons ============//
+global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
+global.phn = '+62 831-7299-9950'
+
+//========== Tampilan Bot =============//
+global.sa = '╭─'
+global.gx = '│✇'
+global.gy = '│•'
+global.gz = '│'
+global.sb = '╰────࿐'
+global.kki = '「'
+global.kka = '」'
+global.zt = '*'
+global.zc = ''
+
+global.thumbnailUrl = [
+  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
+  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
+  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
+  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
+  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+]
+
+//============= Games ================//
+global.benar = '_*Benar✅*_'
+global.salah = '_*Salah❌*_'
+global.dikit = "dikit lagi, semangat ya :')"
+
+global.multiplier = 100 // The higher, The harder levelup
+
+//=========== Requirements ==========//
+
+global.baileys = require('@adiwajshing/baileys')
+global.fs = require('fs')
+global.data = JSON.parse(fs.readFileSync('./data.json'))
+global.fetch = require('node-fetch')
+global.bochil = require('@bochilteam/scraper')
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '🧬',
+      limit: '🌌',
+      healt: '❤️',
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      fishingrod: '🎣',
+      pickaxe: '⛏️',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      iron: '⛓️',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      makananpet: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
